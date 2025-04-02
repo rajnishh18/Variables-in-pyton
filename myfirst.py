4af36bdf0260bb0d206b65f6d50811877e711ea7
Rajnish Kumar Singh <rajnishsingh841427@gmail.com>
2025-04-02 09:51:54 +0530

    Add files via upload

---
+++ myfirst.py
@@ -0,0 +1,27 @@
+name = "Rajnish"
+age = 19
+ad_no ="23SCSE1040191"
+enr_no = 23131040635
+
+#print("My name is :",name)
+#print("My age is:",age)
+print("My name is",name,"and My age is",age,", My admission no. is ", ad_no ,"and Enrollment no. is ",enr_no)
+#print(type(ad_no))
+
+#age2 = age 
+#print (age2)
+
+a = 1900
+b = 15 
+Sum = a+b
+Difference=a-b
+area_of_rectangle = 2*(a+b)
+product = a*b
+divide = a/b
+area_of_square= a*a
+print("The Sum of a+b is :", Sum)
+print("Difference between a and b is ",Difference)
+print("The area of Rectangle is :", area_of_rectangle)
+print("Product of a*b is :",product)
+print("a divide by b is ",divide)
+print("Area of square is ",area_of_square)
